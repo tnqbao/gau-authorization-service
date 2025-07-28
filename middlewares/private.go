599528c33a50779ca49195fb03e288b0af7fb8ea
unknown <quocbao.job106204@gmail.com>
2025-07-28 07:41:30 +0700

    hotfix: abort request in middleware logic

--- middlewares/private.go
+++ middlewares/private.go
@@ -9,12 +9,16 @@ import (
 func PrivateMiddleware(config *config.EnvConfig) gin.HandlerFunc {
 	return func(c *gin.Context) {
 		privateKey := c.GetHeader("Private-Key")
+
 		if privateKey == "" {
 			utils.JSON400(c, "Private key is required")
+			c.Abort()
+			return
 		}
 
 		if privateKey != config.PrivateKey {
 			utils.JSON403(c, "Invalid private key")
+			c.Abort()
 			return
 		}
 
